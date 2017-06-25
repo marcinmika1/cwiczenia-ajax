c4774be364fb3d1efa0a83af4cd880be79429fd8
Marcin Mika <marcinmika@hotmail.com>
2017-06-25 16:43:13 +0200

    zrobiona niekonczaca sie lista pobiaranie z servera i wysciwtlanie

---
+++ js/cwiczenia.js
@@ -0,0 +1,25 @@
+'use strict';
+$(function () {
+    console.log('jq działa')
+
+    $(window).scroll(function () {
+
+        console.log($(window).scrollTop() + $(window).height());
+        console.log($(document).height())
+        /*jesli dochodzimy do konca strony to ...*/
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+
+            $.getJSON('https://jsonplaceholder.typicode.com/users', function (objekt) {
+                /* console.log(objekt);*/
+                for (var i = 0; i < objekt.length; i++) {
+                    $('body').append('<p>Nazwa użytkownika: ' + objekt[i].name + '</p>')
+                    $('body').append('<p>Id użytkownika: ' + objekt[i].id + '</p>')
+                    $('body').append('<p>URL użytkownika:' + objekt[i].website +'</p>'+ '<hr>');
+                    
+                }
+            })
+
+        }
+    })
+
+})
